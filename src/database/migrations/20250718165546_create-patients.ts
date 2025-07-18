08dd415336a8cbc895aac5df1637e3ea15854a58
Possomato <gabrielb.possomato@gmail.com>
2025-07-18 14:02:37 -0300

    add - patients migration

--- src/database/migrations/20250718165546_create-patients.ts
+++ src/database/migrations/20250718165546_create-patients.ts
@@ -1,14 +1,15 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('procedures', (table) => {
+  knex.schema.createTable('patients', (table) => {
     table.increments('id').primary(),
       table.text('name').notNullable(),
-      table.text('description').notNullable(),
-      table.integer('value')
+      table.text('phone'),
+      table.text('email'),
+      table.text('observations')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('procedures')
+  await knex.schema.dropTable('patients')
 }
