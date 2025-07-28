5d7b219093d3e528b1b3a19f813b13e70c5d334a
Possomato <gabrielb.possomato@gmail.com>
2025-07-28 11:11:06 -0300

    add - appointments migration

---
+++ src/database/migrations/20250718183203_create-appointments.ts
@@ -0,0 +1,20 @@
+import type { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('appointments', (table) => {
+    table.increments('id').primary()
+    table.integer('id_dentist').references('id').inTable('dentists')
+    table.integer('id_patient').references('id').inTable('patients')
+    table.integer('id_procedure').references('id').inTable('procedure')
+    table.text('date').notNullable(), table.text('status').notNullable()
+    table.text('payment_method')
+    table.text('payment_date')
+    table.text('observations')
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('appointments')
+}
